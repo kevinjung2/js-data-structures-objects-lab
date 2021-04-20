a1cbb300a4bb1d15a544e0962206a1098849ecf1
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-20 08:08:57 -0400

    commit tree d91e8dc860e29b2b3db70be9ebdb9bc3656edc3c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let driver = {}
+
+function updateDriverWithKeyAndValue(object, key, value) {
+  const newDriver = {...object}
+
+  newDriver[key] = value
+  return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = {...driver}
+
+  delete newDriver[key]
+  return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
